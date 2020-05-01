e15fd5842255c4e3f8e91dbf1f98453ee112e8db
Konrad Dąbrowski <konrad.dabrowski@arcaim.pl>
2020-05-01 17:29:04 +0200

    Update travis stuff

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,26 @@
+git clone --depth=50 --branch=master https://github.com/konraddabrowski/blog.spa.git
+cd konraddabrowski/blog.sp
+git checkout -qf master
+
+#export GITHUB_TOKEN=[secure]
+
+nvm install node
+
+# Setting up build cache
+#export CASHER_DIR=${TRAVIS_HOME}/.casher
+
+node --version
+npm --version
+nvm --version
+
+#npm ci
+
+
 # Build vue project
 npm run build
-cd dist
 
-ls -a
+#rvm $(travis_internal_ruby) --fuzzy do ruby -S gem install dpl
+
+cd /home/travis/build/konraddabrowski/blog.spa/dist
+
+#ls -a
