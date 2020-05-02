8e1eda8c4cc68de2aee1906b63db83f65a353da0
Konrad Dąbrowski <konrad.dabrowski@arcaim.pl>
2020-05-02 14:01:10 +0200

    Update travis stuff

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -11,17 +11,6 @@ echo "  Port $FTP_PORT" >> ~/.ssh/config
 echo "  IdentityFile ~/.ssh/githubToServer" >> ~/.ssh/config
 echo "  User $FTP_USER" >> ~/.ssh/config
 chmod 600 ~/.ssh/config
-  
-#echo $GH_TO_SRV >> ~/.ssh/githubToServer
-
-echo "BLE $TEST_AAA" >> ~/.ssh/test_aaa
-echo "TEST $TEST_AAA" >> ~/.ssh/test_aaa
-chmod 600 ~/.ssh/test_aaa
-
-#echo $SRV_CONFIG >> ~/.ssh/config
-#chmod 600 ~/.ssh/config
-
-cat ~/.ssh/test_aaa
 
 #cd ~/build/konraddabrowski/blog.spa/dist
 #ls
