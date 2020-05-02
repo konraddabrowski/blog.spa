3b0b6602110a4d822b75f650a823b15a12878c14
Konrad Dąbrowski <konrad.dabrowski@arcaim.pl>
2020-05-02 13:51:53 +0200

    Update travis stuff

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,13 +1,27 @@
 mkdir -p ~/.ssh
 
-#echo $GH_TO_SRV >> ~/.ssh/githubToServer
-echo $TEST_AAA >> ~/.ssh/githubToServer
+echo "-----BEGIN OPENSSH PRIVATE KEY-----" >> ~/.ssh/githubToServer
+echo $GITHUB_TO_SERVER_KEY >> ~/.ssh/githubToServer
+echo "-----END OPENSSH PRIVATE KEY-----" >> ~/.ssh/githubToServer
 chmod 600 ~/.ssh/githubToServer
+
+echo "Host srv $FTP_HOST" >> ~/.ssh/config
+echo "  HostName $FTP_HOST" >> ~/.ssh/config
+echo "  Port $FTP_PORT" >> ~/.ssh/config
+echo "  IdentityFile ~/.ssh/githubToServer" >> ~/.ssh/config
+echo "  User $FTP_USER" >> ~/.ssh/config
+chmod 600 ~/.ssh/config
+  
+#echo $GH_TO_SRV >> ~/.ssh/githubToServer
+
+echo "BLE $TEST_AAA" >> ~/.ssh/test_aaa
+echo "TEST $TEST_AAA" >> ~/.ssh/test_aaa
+chmod 600 ~/.ssh/test_aaa
 
 #echo $SRV_CONFIG >> ~/.ssh/config
 #chmod 600 ~/.ssh/config
 
-cat ~/.ssh/githubToServer
+cat ~/.ssh/test_aaa
 
 #cd ~/build/konraddabrowski/blog.spa/dist
 #ls
