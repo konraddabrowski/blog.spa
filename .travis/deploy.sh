498905ad2fd8fa3b5addc6911439c7330dd070f8
Konrad Dąbrowski <konrad.dabrowski@arcaim.pl>
2020-05-02 10:00:48 +0200

    Update travis stuff

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -6,6 +6,6 @@ chmod 600 ~/.ssh/githubToServer
 echo SRV_CONFIG >> ~/.ssh/config
 chmod 600 ~/.ssh/config
 
-cp ~/build/konraddabrowski/blog.spa/dist
+cd ~/build/konraddabrowski/blog.spa/dist
 ls
 #scp -r * srv:public_html/blog
